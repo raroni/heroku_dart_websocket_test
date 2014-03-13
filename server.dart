2b3334b4ade30221699c5a90e0af0293620a36e3
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2014-03-13 21:09:45 +0100

    Readying Heroku WebSocket test.

--- server.dart
+++ server.dart
@@ -1,30 +1,42 @@
-import 'dart:io' show File, HttpServer, Platform;
+import 'dart:io' as IO;
 import 'dart:async' show runZoned;
 import 'package:http_server/http_server.dart' show VirtualDirectory;
 
+var staticFiles;
+
 void main() {
-  var environment = Platform.environment['ENV'];
+  var environment = IO.Platform.environment['ENV'];
   if(environment == null) environment = 'development';
   var webDir = environment == 'development' ? 'web' : 'build';
-  var buildUri = Platform.script.resolve(webDir);
+  var buildUri = IO.Platform.script.resolve(webDir);
 
-  var staticFiles = new VirtualDirectory(buildUri.toFilePath());
+  staticFiles = new VirtualDirectory(buildUri.toFilePath());
   if(environment == 'development') {
     staticFiles.jailRoot = false;
   }
   staticFiles.allowDirectoryListing = true;
   staticFiles.directoryHandler = (dir, request) {
     var indexUri = new Uri.file(dir.path).resolve('index.html');
-    staticFiles.serveFile(new File(indexUri.toFilePath()), request);
+    staticFiles.serveFile(new IO.File(indexUri.toFilePath()), request);
   };
 
-  var portEnv = Platform.environment['PORT'];
-  var port = portEnv == null ? 9999 : int.parse(portEnv);
+  var portEnv = IO.Platform.environment['PORT'];
+  var port = portEnv == null ? 3000 : int.parse(portEnv);
 
   runZoned(() {
-    HttpServer.bind('0.0.0.0', port).then((server) {
-      server.listen(staticFiles.serveRequest);
+    IO.HttpServer.bind('0.0.0.0', port).then((server) {
+      server.listen(onRequest);
     });
   },
   onError: (e, stackTrace) => print('Oh noes! $e $stackTrace'));
+}
+
+void onRequest(IO.HttpRequest request) {
+  if(IO.WebSocketTransformer.isUpgradeRequest(request)) {
+    IO.WebSocketTransformer.upgrade(request).then((IO.WebSocket socket) {
+      socket.close();
+    });
+  } else {
+    staticFiles.serveRequest(request);
+  }
 }
--- web/test.dart
+++ web/test.dart
@@ -1,3 +1,14 @@
+import 'dart:html' as HTML;
+
 void main() {
   print("Hello from Dart.");
+  var uri = Uri.parse(HTML.window.location.href);
+  var serverURL = "ws://${uri.host}:${uri.port}";
+  WebSocket socket = new HTML.WebSocket(serverURL);
+  socket.onOpen.listen((Event event) {
+    print("Connection!");
+  });
+  socket.onClose.listen((Event event) {
+    print("Disconnected!");
+  });
 }
