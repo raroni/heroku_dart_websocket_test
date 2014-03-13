c56deb112bcbb69bf298cff28de7bba06cfa77ab
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2014-03-13 21:13:11 +0100

    Bugfix: Have to use HTML namespace to access WebSocket, Event, etc.

--- web/test.dart
+++ web/test.dart
@@ -4,11 +4,11 @@ void main() {
   print("Hello from Dart.");
   var uri = Uri.parse(HTML.window.location.href);
   var serverURL = "ws://${uri.host}:${uri.port}";
-  WebSocket socket = new HTML.WebSocket(serverURL);
-  socket.onOpen.listen((Event event) {
+  HTML.WebSocket socket = new HTML.WebSocket(serverURL);
+  socket.onOpen.listen((HTML.Event event) {
     print("Connection!");
   });
-  socket.onClose.listen((Event event) {
+  socket.onClose.listen((HTML.Event event) {
     print("Disconnected!");
   });
 }
